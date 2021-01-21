5d41129f1887d9d4254507333fe8cdc98207d815
unknown <igboekwulusifranklin@gmail.com>
2021-01-21 15:25:18 +0100

    initial commit

---
+++ src/com/kraft/Controller.java
@@ -0,0 +1,5 @@
+package com.kraft;
+
+public class Controller {
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module Electrical.Estimate.App {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens com.kraft;
+}
