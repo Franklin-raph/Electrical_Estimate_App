fd4703625abff94e02edc65a72c8fdf7b05a3b28
unknown <igboekwulusifranklin@gmail.com>
2021-01-21 17:37:46 +0100

    add an icon for the console run

--- src/com/kraft/Main.java
+++ src/com/kraft/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -14,8 +15,9 @@ public class Main extends Application {
         primaryStage.initStyle(StageStyle.UNDECORATED);
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/com/kraft/image/kr.png")));
     }
 
 
