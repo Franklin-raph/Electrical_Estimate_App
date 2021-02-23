c320ce5c34f98c9e3905ec473195db4c7c66c0f9
EbubeCode <61353651+EbubeCode@users.noreply.github.com>
2021-02-23 18:20:03 +0100

    create homeStages

---
+++ src/kraft/app/ui/home/HomeStageController1.java
@@ -0,0 +1,64 @@
+package kraft.app.ui.home;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import kraft.app.util.Handlers;
+
+import java.io.File;
+
+public class HomeStageController1 {
+    @FXML
+    private Label closeLabel;
+
+    @FXML
+    private AnchorPane topPane, mainPane;
+
+    public void initialize() {
+        Platform.runLater(() -> {
+            addHandlersForClose();
+            topPane.prefWidthProperty().bindBidirectional(mainPane.prefWidthProperty());
+            //topPane.fitWidthProperty().bind(videoPane.widthProperty());
+        });
+    }
+
+    private void addHandlersForClose() {
+        closeLabel.setOnMouseEntered(event -> {
+            Handlers.getInstance().handleMouseEntered(closeLabel);
+        });
+
+    closeLabel.setOnMouseExited(event -> {
+                Handlers.getInstance().handleMouseExited(closeLabel);
+            });
+
+    closeLabel.setOnMouseClicked(event -> {
+                Handlers.getInstance().handleClose();
+            });
+        }
+
+        @FXML
+        public void CreateNewProject(){
+            Dialog dialog = new Dialog();
+            dialog.setTitle("Create new project");
+        }
+
+        @FXML
+        public void About(){
+
+        }
+
+        @FXML
+        public void OpenProject(){
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Create File");
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter(
+                    "Kraft Files", "*.kr"
+            ));
+            File selectedFile = fileChooser.showOpenDialog(
+                    mainPane.getScene().getWindow()
+            );
+        }
+}
