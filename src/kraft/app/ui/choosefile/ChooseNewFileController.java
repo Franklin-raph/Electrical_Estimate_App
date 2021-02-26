ae040aa0e2398bca3547b7ff5634227977cbcd53
EbubeCode <61353651+EbubeCode@users.noreply.github.com>
2021-02-26 18:12:42 +0100

    add icon to ChooseNewFile

--- src/kraft/app/ui/choosefile/ChooseNewFileController.java
+++ src/kraft/app/ui/choosefile/ChooseNewFileController.java
@@ -1,4 +1,37 @@
 package kraft.app.ui.choosefile;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import kraft.app.util.Handlers;
+
 public class ChooseNewFileController {
+    @FXML
+    private Label closeLabel;
+
+    @FXML
+    private AnchorPane topPane, mainPane;
+
+    public void initialize() {
+        Platform.runLater(() -> {
+            addHandlersForClose();
+            topPane.prefWidthProperty().bindBidirectional(mainPane.prefWidthProperty());
+            //topPane.fitWidthProperty().bind(videoPane.widthProperty());
+        });
+    }
+
+    private void addHandlersForClose() {
+        closeLabel.setOnMouseEntered(event -> {
+            Handlers.getInstance().handleMouseEntered(closeLabel);
+        });
+
+        closeLabel.setOnMouseExited(event -> {
+            Handlers.getInstance().handleMouseExited(closeLabel);
+        });
+
+        closeLabel.setOnMouseClicked(event -> {
+            Handlers.getInstance().handleClose();
+        });
+    }
 }
